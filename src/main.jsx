944cf6a60567595affb901f25be6e8fb702258b6
marcoruz <marco.ruzzo06@googlemail.com>
2024-05-04 00:41:26 +0200

    bug Fix_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // 
+import App from './App'; // Neu
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
